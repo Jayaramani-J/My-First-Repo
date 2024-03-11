5cf119f12ed0009c1e319bbc2ed6bf70262d93ec
Jaya <147045284+Jayaramani-J@users.noreply.github.com>
2024-03-11 13:09:09 +0530

    Add files via upload

---
+++ Jayaramani/Projects/Streamlit App/app.py
@@ -0,0 +1,33 @@
+import streamlit as st
+st.title("Welcome to my BangleShop")
+
+st.write("""
+    My bangle shop offers a beautiful collection of bangles made from various materials and styles.
+    Explore our collection below!
+""")
+
+# About Us section
+st.subheader("About Us")
+st.write("""
+    We have been in the business of selling beautiful bangles for over 10 years. Our mission is to provide
+    high-quality bangles that add elegance and charm to your style.
+""")
+   
+st.subheader("Contact Us")
+st.write("""
+    If you have any questions or would like to place an order, feel free to reach out to us!
+""")
+st.write("Email: minubangles@gmail.com")
+st.write("Phone: +1234567890")
+
+
+
+# Social media links
+st.subheader("Connect With Us")
+st.write("""
+    Follow us on social media for the latest updates and promotions:
+    - [Facebook](https://www.facebook.com/)
+    - [Instagram](https://www.instagram.com/)
+    - [Twitter](https://twitter.com/)
+""")
+
